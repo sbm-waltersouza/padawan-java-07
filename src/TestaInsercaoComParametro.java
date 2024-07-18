14fbd1feee4c001043d8b62cc773e16d32fce3f1
sbm-waltersouza <169094760+sbm-waltersouza@users.noreply.github.com>
2024-07-18 14:34:23 -0300

    Lidando com commit e rollback

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -8,18 +8,34 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 
-		String nome = "Teclado";
-		String descricao = "Teclado com fio";
-
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
+		connection.setAutoCommit(false);
 
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
-				Statement.RETURN_GENERATED_KEYS);
+		try {
+			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 
+			adicionarVariavel("SmartTV", "45 polegadas", stm);
+			adicionarVariavel("Radio", "Radio de bateria", stm);
+			
+			connection.commit();
+			
+			stm.close();
+			connection.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ROLLBACK EXECUTADO");
+			connection.rollback();
+		}
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 
+		
 		stm.execute();
 
 		ResultSet rst = stm.getGeneratedKeys();
@@ -29,5 +45,6 @@ public class TestaInsercaoComParametro {
 			System.out.println("O id criado foi: " + id);
 		}
 
+		rst.close();
 	}
 }
