3dff86fead07d67f6369b9ebe04631ee609ae6da
sbm-waltersouza <169094760+sbm-waltersouza@users.noreply.github.com>
2024-07-18 14:58:11 -0300

    Usando o try-with-resources

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -12,39 +12,35 @@ public class TestaInsercaoComParametro {
 		Connection connection = factory.recuperarConexao();
 		connection.setAutoCommit(false);
 
-		try {
-			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
+		try (PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+				Statement.RETURN_GENERATED_KEYS)) {
 
 			adicionarVariavel("SmartTV", "45 polegadas", stm);
 			adicionarVariavel("Radio", "Radio de bateria", stm);
-			
+
 			connection.commit();
-			
-			stm.close();
-			connection.close();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("ROLLBACK EXECUTADO");
 			connection.rollback();
 		}
+		connection.close();
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 
-		
 		stm.execute();
 
-		ResultSet rst = stm.getGeneratedKeys();
+		try (ResultSet rst = stm.getGeneratedKeys()) {
 
-		while (rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O id criado foi: " + id);
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id criado foi: " + id);
+			}
+
 		}
-
-		rst.close();
 	}
 }
--- src/TestaRemocao.java
+++ src/TestaRemocao.java
@@ -1,7 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 
 public class TestaRemocao {
